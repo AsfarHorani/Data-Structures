acf21730bf2b18b55af55b401f9f9e608b0bffe9
asfarhorani <asfarhorani@gmail.com>
2021-11-05 21:37:35 +0500

    pointers start

--- Pointers/0. Pointers bsics/main.cpp
+++ Pointers/0. Pointers bsics/main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
+
+class swap{
+    public:
+   static void swapping(int *a, int *b){
+        cout << *a << endl;
+        cout << *b << endl;
+    int c ;
+    c=*a;
+    *a=*b;
+    *b = c;
+       cout << *a << endl;
+        cout << *b << endl;
+
+}
+
+};
+
+
+int main(void)
 {
+
+
     int *pi;
     int s=7;
+    int t = 6;
     pi=&s;
 
     cout << *pi << endl;
@@ -16,5 +36,7 @@ int main()
        cout << *pi << endl;
 
       cout << pi << endl;
+
+      swap::swapping(&s,&t);
     return 0;
 }
